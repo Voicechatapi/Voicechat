2a7e50ea0e269602093d9d3b412704efebc1bbe8
Spriteson <duan_hui@hotmail.com>
2023-07-27 02:22:51 +0800

    error after register

--- voicechat/base/views.py
+++ voicechat/base/views.py
@@ -1,69 +1,82 @@
-from django.shortcuts import render
+from typing import Any, Dict
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
-
 from .models import Task
-from django.shortcuts import redirect
 
-
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+    
 class RegisterPage(FormView):
-    template_name = "base/register.html"
+    template_name = 'base/register.html'
     form_class = UserCreationForm
     redirect_authenticated_user = True
-    success_url = reverse_lazy("index")
-
+    success_url = reverse_lazy('tasks')
+    
     def form_valid(self, form):
         user = form.save()
         if user is not None:
             login(self.request, user)
         return super(RegisterPage, self).form_valid(form)
-
+    
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
-            return redirect('index')
+            return redirect('tasks')
         return super(RegisterPage, self).get(*args, **kwargs)
+    
+    
 
-
-class CustomLoginView(LoginView):
-    template_name = "base/custom_login.html"
-    fields='__all__'
-    redirect_authenticated_user = True
-
-    def get_success_url(self):
-        return reverse_lazy('index')
-
-# Create your views here.
-class TaskList(PermissionRequiredMixin, ListView):
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count
+        
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__startswith=search_input)
+        
+        context['search_input'] = search_input
+        return context
 
-class TaskDetail(PermissionRequiredMixin,DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
-
-class TaskCreate(PermissionRequiredMixin,CreateView):
+    
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
-    template_name = 'base/create.html'
-    success_url = reverse_lazy('index')
-
-class TaskUpdate(PermissionRequiredMixin,UpdateView):
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate,self).form_valid(form)
+    
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
-    template_name = 'base/create.html'
-    success_url = reverse_lazy('index')
-
-class TaskDelete(PermissionRequiredMixin,DeleteView):
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
+    
+class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
-    fields = '__all__'
-    template_name = 'base/delete.html'
-    success_url = reverse_lazy('index')
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
