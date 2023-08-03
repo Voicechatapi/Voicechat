1a34a3f249d36732c6b30c3b12dbbed986584848
Spriteson <duan_hui@hotmail.com>
2023-08-03 16:41:48 +0800

    can login

--- voicechat/base/urls.py
+++ voicechat/base/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
     path('', StarterView.as_view(), name='starter'),
+    path('interface/', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
